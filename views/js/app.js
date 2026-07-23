2afa9d7459b686c30b615c4dfb280f7ef96db513
Nickleirsch <1231303114@student.mmu.edu.my>
2026-07-23 11:07:27 +0800

    UI Design

---
+++ views/js/app.js
@@ -0,0 +1,8 @@
+/* ========================
+   AEGIS' SHARED JAVASCRIPT
+   ======================== */
+
+/* Slides the side menu in/out and fades the overlay. */
+function toggleMenu() {
+  document.querySelector('.app').classList.toggle('menu-open');
+}
